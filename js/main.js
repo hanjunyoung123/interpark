96a333ed4fdf334a8509351d564537dd6e3ed8ad
hanjunyoung <hjy961017@gamil.com>
2024-07-12 13:21:43 +0900

    슬라이드

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 window.onload = function(){
     // 상단의 마이페이지리스트 기능
-    const click1 = document.querySelector(".mypage-bt");
-    const mypage = document.querySelector(".mypage-list");
-
-    click1.onclick = function(){
-    mypage.classList.toggle("show");
-};
+  const mypageBt = document.querySelector(".mypage-bt")  
+  const mypageList = document.querySelector(".mypage-list")
+  mypageBt.addEventListener("click",function(){
+    mypageList.classList.toggle("showMypage")
+  })
+  // swiper ================
+  // 프로모션 스와퍼기능
+  const swPromotion = new Swiper(".sw-promotion" , {
+    slidesPerView : 1,
+    spaceBetween : 24,
+    breakpoints: {
+        760 : {
+            slidesPerView : 2
+        },
+        480 : {
+            slidesPerView : 2
+        }
+    },navigation:{
+      nextEl:".promotion .sw-next",
+      prevEl:".promotion .sw-prev",
+    },
+    loop : true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  })
 }
