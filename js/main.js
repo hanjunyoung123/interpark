bd88f9b45f62022699824be64dba19c91f21a706
hanjunyoung <hjy961017@gamil.com>
2024-07-16 18:11:30 +0900

    슬라이드넣기

--- js/main.js
+++ js/main.js
@@ -31,17 +31,31 @@ window.onload = function () {
 
  // 쇼핑의 스와이퍼 기능적용 
 const swShopping = new Swiper(".sw-shopping" ,{
-  slidesPerView: 3,
+  navigation: {
+    nextEl: ".shopping .sw-next",
+    prevEl: ".shopping .sw-prev",
+  },
+  loop:true,
+  slidesPerView: 1.8,
   grid:{
-    rows:2,
+    rows:1,
     fill:"row",
   },
   spaceBetween:10,
 
   breakpoints: {
+    320: {
+      spaceBetween: 32,
+        slidesPerView: 1.2,
+        slidesPerGroup: 1,
+        grid:{
+          rows:1,
+          fill:"row"
+        },
+    },
     900: {
       spaceBetween: 32,
-        slidesPerView: 4,
+        slidesPerView: 2,
         slidesPerGroup: 3,
         grid:{
           rows:2,
@@ -67,7 +81,103 @@ const swShopping = new Swiper(".sw-shopping" ,{
         },
     },
   }
- })
-};
+})
+const swTour = new Swiper(".sw-tour" ,{
+    slidesPerView: 3,
+  navigation: {
+    nextEl: ".tour .sw-next",
+    prevEl: ".tour .sw-prev",
+  },
 
-
+  loop:true,
+  grid:{
+    rows:2,
+    fill:"row",
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 32,
+        slidesPerView:1.2,
+        grid:{
+          rows:2,
+          fill:"row"
+        },
+    },
+    900: {
+      spaceBetween: 32,
+        slidesPerView:2,
+        grid:{
+          rows:6,
+          fill:"row"
+        },
+    },
+    1024:{
+      spaceBetween: 32,
+        slidesPerView: 2,
+        slidesPerGroup: 3,
+        grid:{
+          rows:1,
+          fill:"row"
+        },
+    },
+    1280:{
+      spaceBetween: 26,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid:{
+          rows:1,
+          fill:"row"
+        },
+    },
+  }
+});
+// 티켓 스와이퍼
+const swTikcet = new Swiper(".sw-ticket" ,{
+    slidesPerView: 3,
+  navigation: {
+    nextEl: ".ticket .sw-next",
+    prevEl: ".ticket .sw-prev",
+  },
+  loop:true,
+  grid:{
+    rows:2,
+    fill:"row"
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 32,
+        slidesPerView:1.2,
+        grid:{
+          rows:2,
+          fill:"row"
+        },
+    },
+    900: {
+      spaceBetween: 32,
+        slidesPerView:2,
+        grid:{
+          rows:6,
+          fill:"row"
+        },
+    },
+    1024:{
+      spaceBetween: 32,
+        slidesPerView: 2,
+        slidesPerGroup: 3,
+        grid:{
+          rows:1,
+          fill:"row"
+        },
+    },
+    1280:{
+      spaceBetween: 26,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid:{
+          rows:1,
+          fill:"row"
+        },
+    },
+  }
+});
+}
